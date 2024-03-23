0f4ae9d1d76bded332579527ee4e3a54becf5655
Gaston <gastonallerdev@gmail.com>
2024-03-23 19:32:34 -0300

    ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import authRoutes from './routes/authRoutes'
+import usersRoutes from "./routes/userRoutes"
 dotenv.config()
 
 const app = express()
@@ -9,6 +10,7 @@ app.use(express.json())
 //Routes 
 
 app.use('/auth',authRoutes)
+app.use('/users',usersRoutes)
 
 
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,36 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+} from "../controllers/usersController";
 
 const router = express.Router();
+const JWT_SECRET = process.env.JWT_SECRET || "default-secret";
 
 //middleware jwt
-const authenticationToken = () => {};
+const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "No autorizado" });
+  }
 
-router.post("/", authenticationToken, () => {});
-router.get("/", authenticationToken, () => {});
-router.get("/:id", authenticationToken, () => {});
-router.put("/:id", authenticationToken, () => {});
-router.delete("/:id", authenticationToken, () => {});
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.error("Error en la autenticación: ", err);
+      return res.status(403).json({ error: "No tienes acceso a este recurso" });
+    }
+
+    next();
+  });
+};
+router.post("/", authenticateToken, createUser);
+router.get("/", authenticateToken, getAllUsers);
+router.get("/:id", authenticateToken, getUserById);
+router.put("/:id", authenticateToken, updateUser);
+router.delete("/:id", authenticateToken, deleteUser);
 export default router;
