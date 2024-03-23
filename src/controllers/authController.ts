ec5c871d0f65e05035583849eee1add79c45aa27
Gaston <gastonallerdev@gmail.com>
2024-03-23 17:49:22 -0300

    AuthControllers

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,44 @@
-import { Request, Response } from "express";
+import  { Request, Response } from "express";
+import { comparePasswords, hashPassword } from "../services/password.service";
+import prisma from "../models/user";
+import { generateToken } from "../services/auth.service";
 
-
-
-
-export const register = async(req:Request,res:Response) => {
-        const {email,password} = req.body
-        try{
-                const hashedPassword = ''
-        }catch(error){
-
-        }
+export const register = async (req: Request, res: Response): Promise<void> => {
 }
+
+//////login
+export const login = async (req: Request, res: Response): Promise<void> => {
+
+        const { email, password } = req.body
+    
+        try {
+    
+            if (!email) {
+                res.status(400).json({ message: 'El email es obligatorio' })
+                return
+            }
+            if (!password) {
+                res.status(400).json({ message: 'El password es obligatorio' })
+                return
+            }
+    
+            const user = await prisma.findUnique({ where: { email } })
+            if (!user) {
+                res.status(404).json({ error: 'Usuario no encontrado' })
+                return
+            }
+    
+            const passwordMatch = await comparePasswords(password, user.password);
+            if (!passwordMatch) {
+                res.status(401).json({ error: 'Usuario y contraseñas no coinciden' })
+            }
+    
+            const token = generateToken(user)
+            res.status(200).json({ token })
+    
+    
+        } catch (error: any) {
+            console.log('Error: ', error)
+        }
+    
+    }
